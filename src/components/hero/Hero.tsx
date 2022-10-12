8a0dfbb9292e63fa2bcdd00aab4f052a4dae83d2
OthmaneIGAO <othmane@igotanoffer.com>
2022-10-12 20:46:35 +0000

    dev: hero section

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,36 @@
+import { TypeAnimation } from 'react-type-animation';
+import PersonalImage from '../personal-image/PersonalImage';
+import './Hero.scss';
+
+export default function Home() {
+  const subTitles = [
+    "I build things for the web.",
+    1500,
+    "I make the web a good place.",
+    1500,
+    "I make dreams come true.",
+    1500
+  ]
+
+  return (
+    <div className="hero-container">
+      <div className="info">
+        <p>Hi, my name is</p>
+        <h1>
+          <span className="name">Othmane Lahrimi</span><br/>
+          <span className="specialty">I'm a Full Stack Developer</span>
+        </h1>
+        <TypeAnimation
+          sequence={subTitles}
+          speed={60}
+          wrapper="h2"
+          cursor={true}
+          repeat={Infinity}
+          className="sub-title"
+        />
+      </div>
+
+      <PersonalImage />
+    </div>
+  )
+}
