99b3bd5f055c7581ba6cdf6bb292b3b2b539aa4c
OthmaneIGAO <othmane@igotanoffer.com>
2022-10-14 08:32:31 +0000

    dev: Images and Files

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,34 @@
+import AngularImg from './angular.png';
+import VueImg from './vue.png';
+import ReactImg from './react.png';
+import PythonImg from './python.png';
+import DjangoImg from './django.png';
+import NodeJsImg from './nodejs.png';
+import ExpressJsImg from './expressjs.png';
+import SqlServerImg from './sql_server.png';
+import PostgreSqlImg from './postgresql.png';
+import MongoDbImg from './mongodb.png';
+import TypescriptImg from './typescript.png';
+import HtmlImg from './html.png';
+import CssImg from './css.png';
+import JavaScriptImg from './javascript.png';
+import ScssImg from './scss.png';
+
+
+export {
+  AngularImg,
+  VueImg,
+  ReactImg,
+  PythonImg,
+  DjangoImg,
+  NodeJsImg,
+  ExpressJsImg,
+  SqlServerImg,
+  PostgreSqlImg,
+  MongoDbImg,
+  TypescriptImg,
+  HtmlImg,
+  CssImg,
+  JavaScriptImg,
+  ScssImg,
+}
