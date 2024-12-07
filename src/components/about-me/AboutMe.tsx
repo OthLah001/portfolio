0d59929e4dfc824c6af6475de24386b3bd8b0fc8
OthLah001 <othmane.lahrimi.work@gmail.com>
2024-12-07 08:41:22 +0000

    feat: refine teh about me section

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -11,22 +11,19 @@ export default function AboutMe() {
 
       <div className="content">
         <p className="bio">
-          I have more than 3 years of experience and a great deal of knowledge in web development technologies: <span className="skill">Angular</span>,
-          <span className="skill"> Vue</span>, <span className="skill">React</span>, <span className="skill">Ionic</span>, <span className="skill">JavaScript</span>,
-          <span className="skill"> Typescript</span>, <span className="skill">JQuery</span>, <span className="skill">Scss</span>, <span className="skill">Python</span>,
-          <span className="skill"> Django</span>, <span className="skill">Node.js</span>, <span className="skill">Express.js</span>, <span className="skill">SQL Server</span>,
-          <span className="skill"> PostgreSQL</span> and <span className="skill">MongoDB</span>. I'm also completely familiar with <span className="skill">AWS</span>,
-          <span className="skill"> Heroku</span>, <span className="skill">Azure</span>, <span className="skill">Git</span>, <span className="skill">Github</span>,
-          <span className="skill"> Twilio</span>, <span className="skill">Timekit</span> and <span className="skill">SyncFusion</span>.<br/><br/>
-          There are several strong points I would like to share with you. First of all, I am able to <span className="skill">quickly understand what 
-          the client wants</span> and what tasks I am required to develop. Besides, I have the potential to keep my patience and 
-          determination for <span className="skill">searching for different methods, strategies, and effective solutions for solving problems</span>. I also 
-          love to divide big operations and tasks into multi sub-tasks in order to <span className="skill">track my progress and provide a good quality of code.</span><br/><br/>
-          My inbox is always open. Whether you have a question, a new opportunity or just want to say hi, I’ll try my best to get back to you!
+          With over 5 years of experience in web development, I have deep expertise in <span className="skill">Angular</span>, <span className="skill">Django</span>, <span className="skill">Python</span>, <span className="skill">TypeScript</span>, <span className="skill">PostgreSQL</span>, and <span className="skill">MongoDB</span>. I also have experience working with <span className="skill">Vue</span>, <span className="skill">React</span>, <span className="skill">Ionic</span>, <span className="skill">Node.js</span>, <span className="skill">Express.js</span>, and <span className="skill">SQL Server</span>. I am well-versed in tools and platforms such as <span className="skill">AWS</span>, <span className="skill">Heroku</span>, <span className="skill">Azure</span>, <span className="skill">Git</span>, <span className="skill">GitHub</span>, <span className="skill">Twilio</span>, <span className="skill">Timekit</span>, and <span className="skill">SyncFusion</span>.
+          <br/><br/>
+          In addition to being a skilled developer, I’m a content creator, sharing knowledge about programming and development across platforms like YouTube, Instagram, and LinkedIn. My goal is to educate and inspire others in the tech community.
+          <br/><br/>
+          I’ve also honed strong communication skills, earning certifications such as the STAGE Academy - Communication Skills Certificate and completing the prestigious McKinsey Forward Program.
+          <br/><br/>
+          What sets me apart is my ability to quickly understand client needs and translate them into effective solutions. I excel at breaking down complex tasks into manageable sub-tasks, ensuring high-quality results.
+          <br/><br/>
+          My inbox is always open. Whether you have a question, a new opportunity, or just want to connect, I’d love to hear from you!
         </p>
 
         <div className="cta">
-          <Button text="Contact me" href="mailto:othmane.lahrimi.work@gmail.com" />
+          <Button text="Get in Touch" href="mailto:me@othmanelahrimi.com " />
           <Button text="Resume" href="https://othmanelahrimi.com/Othmane_LAHRIMI_Resume.pdf" />
         </div>
       </div>
